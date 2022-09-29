d0555f37f72b22e3613dce5e3e8d24aa52f0a68a
jcluff22 <jack8492@gmail.com>
2022-09-29 05:02:47 +0100

    first deployment

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, render_template
-from database_update import main_update, db_connect, get_collection, add_document, main_update
+from database_update import db_connect, get_collection, add_document
+from werkzeug.datastructures import ImmutableMultiDict
 import json
+import pymongo
 
 app = Flask(__name__, static_url_path='')
 
 db_name = "ife-charity-mongodb"
+CONNECTION_STRING = 'mongodb://ife-charity-mdb:gyTxoa3F2HoN6Iwb2mhOwpjkTVl4wa6HrzDODrdNHYT8HAQCIybrIUM7gdN6LWgDpLkAAGmh1auuaTZpXG2RnQ==@ife-charity-mdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@ife-charity-mdb@' 
 
 @app.route("/")
 def home():
@@ -14,18 +17,32 @@ def home():
 
 @app.route("/school", methods=['POST'])
 def school():
-    data = request.form['input_form']
-    main_update(data, db_name, "school")
-    
+    data = request.form.to_dict(flat=False)
+    documentid = main_update(data, db_name, "school")
+    return render_template('success.html')
 
 @app.route("/child", methods=['POST'])
 def child():
-    data = request.form['input_form']
+    data = request.form.to_dict(flat=False)
+    documentid = main_update(data, db_name, "school")
     main_update(data, db_name, "child")
     
 
 @app.route("/sponsor", methods=['POST'])
 def sponsor():
-    data = request.form['input_form']
+    data = request.form.to_dict(flat=False)
+    documentid = main_update(data, db_name, "school")
     main_update(data, db_name, "sponsor")
-    
+
+def main_update(inputjson, db_name, collection_name):
+    """Connect to the API for MongoDB, create DB and collection, perform CRUD operations"""
+    client = pymongo.MongoClient(CONNECTION_STRING)
+    try:
+        client.server_info() # validate connection string
+    except pymongo.errors.ServerSelectionTimeoutError:
+        raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
+
+    db = db_connect(client, db_name)
+    collection = get_collection(db, collection_name)
+    document_id = add_document(collection, inputjson)
+    return document_id
--- database_update.py
+++ database_update.py
@@ -2,9 +2,9 @@ import getpass
 import json
 import pymongo
 
-from random import randint
-
-CONNECTION_STRING = 'xxx' 
+db_name = 'ife-charity-mongodb'
+CONNECTION_STRING = 'mongodb://ife-charity-mdb:gyTxoa3F2HoN6Iwb2mhOwpjkTVl4wa6HrzDODrdNHYT8HAQCIybrIUM7gdN6LWgDpLkAAGmh1auuaTZpXG2RnQ==@ife-charity-mdb.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@ife-charity-mdb@' 
+collection_name = 'test1'
 
 def db_connect(client, db_name):
     db = client[db_name]
@@ -19,7 +19,7 @@ def add_document(collection, input_data):
     print("Inserted document with _id {}".format(document_id))
     return document_id
 
-def main_update(inputjson, db_name, collection_name):
+def main():
     """Connect to the API for MongoDB, create DB and collection, perform CRUD operations"""
     client = pymongo.MongoClient(CONNECTION_STRING)
     try:
@@ -27,9 +27,13 @@ def main_update(inputjson, db_name, collection_name):
     except pymongo.errors.ServerSelectionTimeoutError:
         raise TimeoutError("Invalid API for MongoDB connection string or timed out when attempting to connect")
 
+    inputjson = {"test":"test1","test2":"test2"}
     db = db_connect(client, db_name)
     collection = get_collection(db, collection_name)
     document_id = add_document(collection, inputjson)
 
     # collection = create_database_unsharded_collection(client)
     # document_id = insert_sample_document(collection)
+
+if __name__ == "__main__":
+    main()
