60d058f684a1bab49efbc880a26a32339fe98a57
jcluff22 <jack8492@gmail.com>
2022-09-29 04:23:01 +0100

    first deployment

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
-from database_update import main_update
+from flask import Flask, request, send_from_directory
+from database_update import main_update, db_connect, get_collection, add_document, main_update
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='web/static')
 
 db_name = "ife-charity-mongodb"
 
 @app.route("/")
 def home():
-    return "Hello"
+    return app.send_static_file('form.html')
 
 @app.route("/school", methods=['POST'])
 def school():
@@ -27,5 +27,5 @@ def child():
 @app.route("/sponsor", methods=['POST'])
 def sponsor():
     data = request.form['input_form']
-    main_update(data, db_name, "child")
+    main_update(data, db_name, "sponsor")
     
